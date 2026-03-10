8cde68f90dcfdffefa4ee00d7a857366be38461b
tharikDev13 <tharik.batcha@imaginemarketingindia.com>
2026-03-10 17:48:14 +0530

    Initial setup commit.

---
+++ edgeTest/edgeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  edgeTest
+//
+//  Created by Tharik anver  on 10/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
